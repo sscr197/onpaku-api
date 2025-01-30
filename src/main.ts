cdc6f92a0b17665c1cbb184d1ecf4118097a6e1e
sscr197 <sscr0827@gmail.com>
2025-01-30 14:44:15 +0900

    chore: Add API_KEY and APP_NAME environment variables to Dockerfile and update default port

--- src/main.ts
+++ src/main.ts
@@ -49,7 +49,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  const port = process.env.PORT ?? 3000;
+  const port = process.env.PORT ?? 8080;
   await app.listen(port);
   logger.log(`Application is running on: http://localhost:${port}`);
 }
