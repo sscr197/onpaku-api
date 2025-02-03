f989601a6df26d03ad33e52f1ea0b414251737b2
sscr197 <sscr0827@gmail.com>
2025-02-04 07:57:30 +0900

    feat: Import LoggerModule in FirestoreModule to enable custom logging

--- src/shared/firestore/firestore.module.ts
+++ src/shared/firestore/firestore.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { FirestoreProvider } from './firestore.provider';
+import { LoggerModule } from '../logger/logger.module'; // 追加
 
 @Module({
+  imports: [LoggerModule], // LoggerModule をインポートして、CustomLogger を利用できるようにする
   providers: [FirestoreProvider],
   exports: [FirestoreProvider],
 })
